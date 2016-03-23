786e04ed4d4af56d4f02604abcd7215ac02c1c2a
Gabriel <gabrielc007@gmail.com>
2016-03-23 00:11:47 -0400

    changed header phrase

--- contact_me.php
+++ contact_me.php
@@ -14,8 +14,8 @@ $name = $_POST['name'];
 $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
+
 	
-// Create the email and send the message
 $to = 'gabrielc007@gmail.com'; 
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
