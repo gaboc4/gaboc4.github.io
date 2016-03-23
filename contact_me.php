29cb77e9e3276ab9b11bdfa0e7cbb47779ed9c45
Gabriel <gabrielc007@gmail.com>
2016-03-23 12:53:58 -0400

    change php

--- contact_me.php
+++ contact_me.php
@@ -1,26 +1,27 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+if(empty($_GET['name'])  		||
+   empty($_GET['email']) 		||
+   empty($_GET['phone']) 		||
+   empty($_GET['message'])	||
+   !filter_var($_GET['email'],FILTER_VALIDATE_EMAIL))
    {
 	echo "No arguments Provided!";
 	return false;
    }
-	
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
 
-	
-$to = 'gabrielc007@gmail.com'; 
+$name = $_GET['name'];
+$email_address = $_GET['email'];
+$phone = $_GET['phone'];
+$message = $_GET['message'];
+
+
+$to = 'gabrielc007@gmail.com';
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; 
-$headers .= "Reply-To: $email_address";	
+$headers = "From: noreply@yourdomain.com\n";
+$headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
-return true;			
+echo "test";
+return true;
 ?>
